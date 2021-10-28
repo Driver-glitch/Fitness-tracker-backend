6a5c145c75da338d57790c64dc989f64903d09de
Daniel Rivera <85755386+Driver-glitch@users.noreply.github.com>
2021-10-27 21:17:22 -0400

    Finished building DB

--- db/seedData.js
+++ db/seedData.js
@@ -1,7 +1,10 @@
 // require in the database adapter functions as you write them (createUser, createActivity...)
 // const { } = require('./');
 
+const {createUser} = require("./");
+
 const client = require("./client");
+
 
 async function dropTables() {
   console.log("Dropping All Tables...");
@@ -29,28 +32,29 @@ async function createTables() {
   try {
     await client.query(`
     CREATE TABLE users (
-      Id SERIAL PRIMARY KEY,
+      id SERIAL PRIMARY KEY,
       username VARCHAR(255) UNIQUE NOT NULL,
       password VARCHAR(255) NOT NULL
       );
       CREATE TABLE activities (
-        Id SERIAL PRIMARY KEY,
+        id SERIAL PRIMARY KEY,
         name	VARCHAR(255) UNIQUE NOT NULL,
         description	TEXT NOT NULL
     );
     CREATE TABLE routines (
-      Id SERIAL PRIMARY KEY,
-      "creatorId" INTEGER FOREIGN KEY,
+      id SERIAL PRIMARY KEY,
+      "creatorId" INTEGER REFERENCES users(id),
       "isPublic" BOOLEAN DEFAULT false,
       name VARCHAR(255)	UNIQUE NOT NULL,
       goal TEXT NOT NULL
     );
-    CREATE TABLES routine_activities(
-      Id	SERIAL PRIMARY KEY,
-    "routineId" INTEGER	FOREIGN KEY,
-    "activityId" INTEGER FOREIGN KEY,
+    CREATE TABLE routine_activities(
+      id	SERIAL PRIMARY KEY,
+    "routineId" INTEGER REFERENCES routines(id),
+    "activityId" INTEGER REFERENCES activities(id),
     duration INTEGER,
-    count INTEGER
+    count INTEGER, 
+    UNIQUE("routineId","activityId")
       );
     `);
   } catch (error) {
@@ -252,5 +256,5 @@ async function rebuildDB() {
 }
 
 module.exports = {
-  rebuildDB,
+  rebuildDB
 };
