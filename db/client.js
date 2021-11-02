6bc9ad60d759dc224c787d59f9d2925b38e27def
Daniel Rivera <85755386+Driver-glitch@users.noreply.github.com>
2021-11-02 08:49:51 -0400

    created some Routines

--- db/client.js
+++ db/client.js
@@ -2,11 +2,12 @@
 const { Client } = require("pg"); // imports the pg module
 const client = new Client({
   connectionString:
-    process.env.DATABASE_URL || "postgres://localhost:5432/fitness-dev",
+    process.env.DATABASE_URL || "postgres://localhost:5433/fitness-dev",
   ssl:
     process.env.NODE_ENV === "production"
       ? { rejectUnauthorized: false }
       : undefined,
 });
+
 
 module.exports = client;
