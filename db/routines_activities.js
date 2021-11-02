15367b8784c94442b520a1349a38923efcdf2e6c
Daniel Rivera <85755386+Driver-glitch@users.noreply.github.com>
2021-11-02 08:59:30 -0400

    Created Routine_Activities

---
+++ db/routines_activities.js
@@ -0,0 +1,26 @@
+// Do we need a require?
+
+
+async function getRoutineActivityById() {
+
+}
+async function addActivityToRoutine() {
+
+}
+async function updateRoutineActivity() {
+
+}
+async function destroyRoutineActivity() {
+
+}
+async function getRoutineActivitiesByRoutine() {
+
+}
+
+module.exports = {
+    getRoutineActivityById,
+    addActivityToRoutine,
+    updateRoutineActivity,
+    destroyRoutineActivity,
+    getRoutineActivitiesByRoutine
+}
